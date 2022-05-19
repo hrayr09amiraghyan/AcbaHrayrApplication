3c081b4fd1569286b36779b5ab83e7fcb7996b2b
Hrayr.Amiraghyan <hrayr.amiraghyan@acba.am>
2022-05-19 11:55:15 +0400

    created acba checkbox,acba toolbar

---
+++ HrayrAcbaLibrary/src/main/java/com/example/hrayracbalibrary/AcbaToolbar.kt
@@ -0,0 +1,19 @@
+package com.example.hrayracbalibrary
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.Toolbar
+
+class AcbaToolbar : Toolbar {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+}
