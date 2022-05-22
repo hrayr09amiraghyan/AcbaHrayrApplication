ecae4a596e67ca9be8b2908bf0f0082c019440fc
Hrayr.Amiraghyan <hrayr.amiraghyan@acba.am>
2022-05-22 15:49:51 +0400

    improved code

--- app/src/main/java/com/example/acbahrayrapplication/filter/FileDownloadWorker.kt
+++ app/src/main/java/com/example/acbahrayrapplication/filter/FileDownloadWorker.kt
@@ -1,16 +1,9 @@
 package com.example.acbahrayrapplication.filter
 
 import android.content.Context
-import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStream
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URL
-import javax.net.ssl.HttpsURLConnection
+import com.example.acbahrayrapplication.util.DownLoader.downloadJson
 
 class FileDownloadWorker(appContext: Context, workerParams: WorkerParameters) :
     Worker(appContext, workerParams) {
@@ -22,41 +15,4 @@ class FileDownloadWorker(appContext: Context, workerParams: WorkerParameters) :
         return Result.success()
     }
 
-    @Throws(IOException::class)
-    fun downloadJson(strUrl: String?): String {
-        var data = ""
-        var iStream: InputStream? = null
-        var urlConnection: HttpURLConnection? = null
-        try {
-            val url = URL(strUrl)
-
-            // Creating an http connection to communicate with url
-            urlConnection = url.openConnection() as HttpsURLConnection
-
-            // Connecting to url
-            urlConnection.connect()
-
-            // Reading data from url
-            iStream = urlConnection.getInputStream()
-            val br = BufferedReader(
-                InputStreamReader(
-                    iStream
-                )
-            )
-            val sb = StringBuffer()
-            var line: String
-            while (br.readLine().also { line = it } != null) {
-                sb.append(line)
-            }
-            data = sb.toString()
-            br.close()
-        } catch (e: Exception) {
-            Log.e("TAG", "problem downloading JSON", e)
-        } finally {
-            iStream?.close()
-            urlConnection?.disconnect()
-        }
-        Log.e("Json file", data)
-        return data
-    }
 }
